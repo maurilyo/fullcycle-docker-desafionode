7f5c8b5c38fb7c69f081422d959d03d3ff367990
Maurilio Machado <maurilyo@gmail.com>
2022-03-10 16:29:04 -0300

    Primeiro commit do projeto

---
+++ server1/src/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+var nomes;
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+const sql = `INSERT INTO people(name) values('Maurilio')`
+
+const pool = mysql.createPool({
+  connectionLimit: 10,    
+  password: 'root',
+  user: 'root',
+  database: 'nodedb',
+  host: 'db',
+  port: 3306
+}); 
+
+SelectAllElements = () =>{
+  return new Promise((resolve, reject)=>{
+      pool.query('SELECT * FROM people ',  (error, elements)=>{
+          if(error){
+              return reject(error);
+          }
+          return resolve(elements);
+      });
+  });
+};
+
+app.use(cors());
+app.get('/', async (req, res, next) => {
+  try {
+    const resultElements = await SelectAllElements();
+    res.status(200).json({msg : '<h1>Full Cycle Rocks!</h1>', elements: resultElements});
+  }catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/add', (req, res) => {
+  
+  connection.query(sql)
+  connection.end
+  
+  return res.status(200).send({ "message": "Novo registro adicionado" });
+});
+
+app.listen(5001, () => console.log('Rodando na porta 5001'));
