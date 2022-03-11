71ec639e362318b4dc87d41cf19abb926aa09603
Maurilio Machado <maurilyo@gmail.com>
2022-03-11 15:18:54 -0300

    Revert "Corrigindo insersão do 1o registro"
    This reverts commit 94e0048fc60daf242f44fd03eda5f27d6c12cdd6.

--- server1/src/app.js
+++ server1/src/app.js
@@ -49,6 +49,7 @@ app.use(cors());
 app.get('/', async (req, res, next) => {
   try {
     connection.query(sql)
+    connection.end
 
     const resultElements = await SelectAllElements();
     res.status(200).json({msg : '<h1>Full Cycle Rocks!</h1>', elements: resultElements});
